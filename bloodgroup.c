88d41d9de9e516bc1359d3e8fab78572d7f8418b
Kai23111 <jkai23111@gmail.com>
2023-09-20 03:18:10 +0700

    Resolve merge conflict in bloodgroup.c

--- bloodgroup.c
+++ bloodgroup.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 void main()
-{ char Name[40], G[5], answer[10];
+{ char Name[40], G[5], answer[20];
   int i;
-
-  printf("Hello I am A!!!");
-  
   do
   {
     printf("What is your Name? ");
